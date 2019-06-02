d92aec7080130b1e0c7cbaa4e16ecfa6f6613388
Brenda Camacho <brendax98@hotmail.com>
2019-06-02 00:49:19 -0500

    bamazonmanager

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -20,7 +20,7 @@ var displayProducts = function(){
 	connection.query(query, function(err, res){
 		if(err) throw err;
 		var displayTable = new Table ({
-			head: ["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
+			head: ["Item ID", "Product Name", "Category", "Price", "Quantity"],
 			colWidths: [10,25,25,10,14]
 		});
 		for(var i = 0; i < res.length; i++){
